6878d29ad2fc2195ae723abc00c01cd9bdd28194
ahin2457 <dkdls2457@gmail.com>
2024-10-20 20:22:06 +0900

    게시물 상세보기 기능 구현

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -2,21 +2,29 @@ package com.sbs.exam.board;
 
 import java.util.Scanner;
 
+/*
+Todo List
+- [o] /usr/article/detail 명령어 처리
+- [] /urs/article/write 입력시 가장 최신 게시물 노출
+
+ */
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     // while문 바깥의 변수는 while문 입장에서 전역변수
-    int articleLastId = 0;
+    int lastArticleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 텍스트 게시판 v 0.1 ==");
 
     while (true){
-      System.out.printf("명령) ");
+      System.out.printf("명령어 ");
       String cmd = sc.nextLine();
 
 
 
       if(cmd.equals("/urs/article/write") ){
+        System.out.println("== 게시물 상세보기 ==");
         System.out.print("제목 : ");
         String subject =  sc.nextLine();
 
@@ -31,23 +39,35 @@ public class Main {
 
         // 전이증감 연산?
         //  선 증가시킨 값을 변수에다가 집어 넣은 다음에 그 값을 사용
-        int id = ++articleLastId;
+        int id = ++lastArticleLastId;
 
         // Article 객체의 주소가 출력
         Article article = new Article(id,subject,content);
+        // 입력 받고난 게시물의 객체 주소를 lastArticle에 넣어줌
+        lastArticle = article;
+
         //article.id = id;
         //article.subject = subject;
         //article.content = content;
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      }
+      else if(cmd.equals("/usr/article/detail") ){
+        Article article = lastArticle; // 주소값의 연결
 
+        if(article == null){
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue; //밑에 코드 스킵
+        }
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %d\n", article.subject);
+        System.out.printf("내용 : %d\n", article.content);
+        //break; // 반복문 빠져나오기
       }
-      else if(cmd.equals("exit") ){
-        System.out.println("== 자바 텍스트 게시판 종료 ==");
-        break; // 반복문 빠져나오기
-      }
-      else if(cmd.equals("")){
+      else if(cmd.equals("exit")){
         System.out.println("명령어를 입력해주세요.");
       }
       else{
@@ -59,7 +79,7 @@ public class Main {
     }
 
 
-    sc.close();
+    //sc.close();
 
   }
 }
